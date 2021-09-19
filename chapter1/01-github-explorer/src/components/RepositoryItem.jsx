ca31dad272bfdacc1e3ccaf590481d40321c014a
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2021-09-18 23:33:05 -0300

    update

---
+++ chapter1/01-github-explorer/src/components/RepositoryItem.jsx
@@ -0,0 +1,16 @@
+
+
+export function RepositoryItem({repository}) {
+  return (
+    <>
+      <li>
+          <strong> {repository.name ?? 'Default'} </strong>
+          <p> {repository.description} </p>
+
+          <a href={repository.link} target="_blank">
+            Acessar repositório
+          </a>
+        </li>
+    </> 
+  )
+}
