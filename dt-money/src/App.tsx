9beb037ec0e153deb89bc530d6e3e9c51be4783c
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2021-10-09 14:01:44 -0300

    update context

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -2,6 +2,7 @@ import Modal from 'react-modal';
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard'
 import { NewTransactionModal } from './components/NewTransactionModal'
+import { TransactionContext, TransactionProvider } from './TransactionsContext'
 
 import { GlobalStyle} from './styles/global'
 import { useState } from 'react';
@@ -21,7 +22,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       
@@ -31,7 +32,7 @@ export function App() {
       />
 
       <GlobalStyle />
-   </>
+   </TransactionProvider>
   );
 }
 
