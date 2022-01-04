139019396015456fcb75112ebca993079570977c
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2022-01-03 22:26:29 -0300

    update

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,13 +1,13 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const { data: session } = useSession()
+  const [ session ] = useSession()
 
-  console.log(session)
+ 
 
   return session ? (
     <button
--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,17 +1,13 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import Providers from "next-auth/providers"
 
 export default NextAuth({
 
   providers: [
-    GithubProvider({
+    Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      authorization: {
-        params: {
-          scope: 'read:user',
-        },
-      },
+      scope: 'read:user',
     }),
   ],
 })
