3609ce2709eb77a2aee88cce1387fe766cfb90dd
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2022-01-16 21:22:51 -0300

    Update manageSubscription.ts

--- ignews/src/pages/api/_lib/manageSubscription.ts
+++ ignews/src/pages/api/_lib/manageSubscription.ts
@@ -1,3 +1,4 @@
+import { stripe } from './../../../services/stripe';
 import { query as q } from 'faunadb' 
 
 import { fauna } from './../../../services/fauna';
@@ -6,16 +7,34 @@ export async function saveSubscription(
   subscriptionId: string,
   customerId: string,
 ) {
-  console.log(subscriptionId, customerId);
   // Buscar usuario no banco do fauna
-  const useRef = await fauna.query(
-    q.Get(
-      q.Match(
-        q.Index('user_by_stripe_customer_id'),
-        customerId
+  const userRef = await fauna.query(
+    q.Select(
+      "ref",
+      q.Get(
+        q.Match(
+          q.Index('user_by_stripe_customer_id'),
+          customerId
+        )
       )
     )
   )
 
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId)        
+
   //Salvar os dados da subscription no faunadb
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    priceId: subscription.items.data[0].price.id,
+
+  }
+
+  await fauna.query(
+    q.Create(
+      q.Collection('subscriptions'),
+      { data: subscriptionData }
+    )
+  )
 }
